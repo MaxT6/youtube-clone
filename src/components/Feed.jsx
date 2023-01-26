666592ce881b059ec19f0e9cc7fca1c225db54dc
MaxT6 <maximiliant6@gmail.com>
2023-01-26 15:21:35 -0500

    created SideBar component and imported it to Feed component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import SideBar from './SideBar';
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{
+      flexDirection: { 
+        sx: "column", 
+        md: "row"
+      }
+    }}>
+      <Box sx={{
+        height: {
+          sx: 'auto', // Don't forget that auto will collaspe the div to invisibility when nothing is inside. 
+          md: '92vh'
+        },
+        borderRight: '1px solid #3d3d3d', 
+        px: { sx: 0, md: 2 }
+      }}>
+        <SideBar />
+
+        <Typography className='copyright'
+        variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
+          Copyright 2023 MT Media
+        </Typography>
+      </Box>
+    </Stack>
+
   )
+
 }
 
 export default Feed
