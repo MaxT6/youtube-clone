ff88dbb0e2cf877a037ebcc435c3814076e1db18
MaxT6 <maximiliant6@gmail.com>
2023-01-30 11:11:15 -0500

    Thumbnails are now visible in feed of app.

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
-//This file is to condense the number of imports into one line
+// This file is to condense the number of imports into one line
 // Can be seen in App.js and Feed.jsx
+// Also used in Videos.jsx
 
 export { default as NavBar } from "./NavBar";
 export { default as Feed } from "./Feed";
@@ -9,3 +10,5 @@ export { default as SearchFeed } from "./SearchFeed";
 export { default as SearchBar } from "./SearchBar";
 export { default as SideBar } from "./SideBar";
 export { default as Videos } from "./Videos";
+export { default as VideoCard } from "./VideoCard";
+export { default as ChannelCard } from "./ChannelCard";
